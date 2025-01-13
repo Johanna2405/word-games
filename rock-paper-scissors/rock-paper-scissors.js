bf298bf2c4afcb5f1f5d8cb3aa33889275047ac4
Johanna <johanna.k.1996@googlemail.com>
2025-01-13 20:04:51 +0100

    more efficiant logic and bugfix for validation userInput

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -7,93 +7,86 @@
 // node rockPaperScissors.js rock
 // # Output: You chose rock. Computer chose scissors. You win!
 
-// 1. Get User Input
+// Get User Input & options
 
 const args = process.argv.slice(2);
-
-// check if the user provided exactly one element
-if (args.length !== 1) {
-  console.error("Please provide exactly one option: rock, paper or scissors");
-  return;
-}
-
-// create var from input
 const userInput = args[0];
-
-// check if user input is valid option -> later: check case sensitivity !
-if (userInput != ("rock" || "paper" || "scissors")) {
-  console.log("Please provide exactly one option: rock, paper or scissors");
-}
-
-// 2. var for game elements
-// const rock = "rock";
-// const scissors = "scissors";
-// const paper = "paper";
-
 const options = ["rock", "scissors", "paper"];
 
-// results: -1 = lost 0 = draw 1 = win
-// const loose = -1;
-// const draw = 0;
-// const win = 1;
-
-// comupter choice - function?
+// Random computer choice
 
 const computerChoice = options[Math.floor(Math.random() * options.length)];
 
-// switch test
+// check if the user provided exactly one element
+if (args.length !== 1) {
+  console.error("Please provide exactly one option: rock, paper or scissors.");
+  return;
+}
 
-// switch (true) {
-//   case "rock":
-//     return 1;
-//   case "scissors":
-//     return 2;
-//   case "paper":
-//     return 3;
-// }
+// check if user input is valid option -> later: check case sensitivity !
 
-// first attempt
+if (!options.includes(userInput)) {
+  console.log("Please choose between: rock | paper | scissors");
+}
 
-if (userInput === "rock" && computerChoice === "rock") {
-  console.log(
-    "Computer's choice: ",
-    computerChoice,
-    "Result: It is a draw. Try again!"
-  );
-} else if (userInput === "rock" && computerChoice === "scissors") {
-  console.log("Computer's choice: ", computerChoice, "Result: You win!");
-} else if (userInput === "rock" && computerChoice === "paper") {
-  console.log(
-    "Computer's choice: ",
-    computerChoice,
-    "Result: You loose. Try again!"
-  );
-} else if (userInput === "scissors" && computerChoice === "rock") {
-  console.log(
-    "Computer's choice: ",
-    computerChoice,
-    "Result: You loose. Try again!"
-  );
-} else if (userInput === "scissors" && computerChoice === "scissors") {
-  console.log(
-    "Computer's choice: ",
-    computerChoice,
-    "Result: It is a draw. Try again!"
-  );
-} else if (userInput === "scissors" && computerChoice === "paper") {
-  console.log("Computer's choice: ", computerChoice, "Result: You win!");
-} else if (userInput === "paper" && computerChoice === "rock") {
-  console.log("Computer's choice: ", computerChoice, "Result: You win!");
-} else if (userInput === "paper" && computerChoice === "scissors") {
-  console.log(
-    "Computer's choice: ",
-    computerChoice,
-    "Result: You loose. Try again!"
-  );
-} else if (userInput === "paper" && computerChoice === "paper") {
-  console.log(
-    "Computer's choice: ",
-    computerChoice,
-    "Result: It is a draw. Try again!"
-  );
+if (userInput === "rock") {
+  switch (computerChoice) {
+    case "rock":
+      console.log(
+        "Computer's choice: ",
+        computerChoice,
+        "Result: It is a draw. Try again!"
+      );
+      return;
+    case "scissors":
+      console.log("Computer's choice: ", computerChoice, "Result: You win!");
+      return;
+    case "paper":
+      console.log(
+        "Computer's choice: ",
+        computerChoice,
+        "Result: You loose. Try again!"
+      );
+      return;
+  }
+} else if (userInput === "scissors") {
+  switch (computerChoice) {
+    case "rock":
+      console.log(
+        "Computer's choice: ",
+        computerChoice,
+        "Result: You loose. Try again!"
+      );
+      return;
+    case "scissors":
+      console.log(
+        "Computer's choice: ",
+        computerChoice,
+        "Result: It is a draw. Try again!"
+      );
+      return;
+    case "paper":
+      console.log("Computer's choice: ", computerChoice, "Result: You win!");
+      return;
+  }
+} else if (userInput === "paper") {
+  switch (computerChoice) {
+    case "rock":
+      console.log("Computer's choice: ", computerChoice, "Result: You win!");
+      return;
+    case "scissors":
+      console.log(
+        "Computer's choice: ",
+        computerChoice,
+        "Result: You loose. Try again!"
+      );
+      return;
+    case "paper":
+      console.log(
+        "Computer's choice: ",
+        computerChoice,
+        "Result: It is a draw. Try again!"
+      );
+      return;
+  }
 }
