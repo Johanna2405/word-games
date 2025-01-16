0acf1183af010997fc23e58aedf75955584ef6ef
Johanna <johanna.k.1996@googlemail.com>
2025-01-16 18:19:27 +0100

    first commit

---
+++ ceasar-cipher/ceasar-cipher.js
@@ -0,0 +1,32 @@
+// get input
+const args = process.argv.slice(2, 3);
+const inputPhrase = args[0].toLowerCase();
+const inputNumber = args[1];
+const abc = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
